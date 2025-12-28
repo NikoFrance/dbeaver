36fcbb386f761afe7ac8725a411c0cae6f78390f
NikoFrance <94210405+NikoFrance@users.noreply.github.com>
2025-12-28 23:13:32 +0300

    Update PostgreDialect.java

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreDialect.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreDialect.java
@@ -1116,9 +1116,18 @@ public class PostgreDialect extends JDBCSQLDialect implements TPRuleProvider, SQ
                 : CommonUtils.getBoolean(dataSource.getActualConnectionConfiguration().getProviderProperty(PostgreConstants.PROP_DD_PLAIN_STRING), ddPlainDefault);
 
             return new TPRule[] {
-                new SQLDollarQuoteRule(position == RulePosition.PARTITION, true, ddTagIsString, ddPlainIsString),
-                new PostgreEscapeStringRule()
-            };
+    new SQLDollarQuoteRule(
+        position == RulePosition.PARTITION,
+        true,
+        ddTagIsString,
+        ddPlainIsString,
+        ddTagIsString,
+        ddPlainIsString
+    ),
+    new PostgreEscapeStringRule()
+};
+                
+            
         }
         return new TPRule[0];
     }
